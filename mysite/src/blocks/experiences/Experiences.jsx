03dff483bbd34eaa84502f3ff6bb44838c459d94
yahanliu <liuyahan0812@gmail.com>
2025-05-07 14:59:58 +1200

    add background to Experience section

--- mysite/src/blocks/experiences/Experiences.jsx
+++ mysite/src/blocks/experiences/Experiences.jsx
@@ -1,9 +1,7 @@
 'use client';
+
 import PropTypes from 'prop-types';
-
-import { useState } from 'react';
-
-// @next
+import { useMemo, useState } from 'react';
 
 // @mui
 import Accordion from '@mui/material/Accordion';
@@ -21,26 +19,26 @@ import ContainerWrapper from '@/components/ContainerWrapper';
 import FaqDetails from '@/components/faq/FaqDetails';
 import SvgIcon from '@/components/SvgIcon';
 import Typeset from '@/components/Typeset';
-
 import useFocusWithin from '@/hooks/useFocusWithin';
 import { generateFocusVisibleStyles } from '@/utils/CommonFocusStyle';
 import { SECTION_COMMON_PY } from '@/utils/constant';
-
-/***************************  FAQ - 6  ***************************/
 
 export default function Experiences({ heading, caption, defaultExpanded, faqList, activeCategory }) {
   const theme = useTheme();
   const isFocusWithin = useFocusWithin();
   const [expanded, setExpanded] = useState(defaultExpanded || false);
-  const [filterFaqList, setFilterFaqList] = useState(activeCategory ? faqList.filter((item) => item.category === activeCategory) : faqList);
+
+  const filterFaqList = useMemo(() => {
+    return activeCategory ? faqList.filter((item) => item.category === activeCategory) : faqList;
+  }, [faqList, activeCategory]);
 
   const cardRadius = { xs: 4, sm: 6 };
   const accordionRadius = { xs: cardRadius.xs * 4, sm: cardRadius.sm * 4 };
   const accordionPX = { xs: 2, sm: 3 };
-  const iconProps = { color: 'text.primary' };
 
-  // Handles the expansion of accordion panels
-  const handleChange = (panel) => (event, isExpanded) => setExpanded(isExpanded ? panel : false);
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
     <ContainerWrapper sx={{ py: SECTION_COMMON_PY }}>
@@ -49,59 +47,105 @@ export default function Experiences({ heading, caption, defaultExpanded, faqList
           initial={{ opacity: 0, y: 10 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
-          transition={{
-            duration: 0.5,
-            delay: 0.4
-          }}
+          transition={{ duration: 0.5, delay: 0.4 }}
         >
-          <Stack direction={{ sm: 'row' }} sx={{ gap: 4, justifyContent: 'space-between', marginLeft: '40%', alignItems: { xs: 'center', sm: 'end' } }}>
+          <Stack
+            direction={{ sm: 'row' }}
+            sx={{
+              gap: 4,
+              justifyContent: 'space-between',
+              alignItems: { xs: 'center', sm: 'end' },
+              textAlign: { xs: 'center', sm: 'left' },
+            }}
+          >
             <Typeset {...{ heading, caption }} />
           </Stack>
         </motion.div>
+
         <Stack sx={{ gap: 2 }}>
           <Stack
             sx={{
               gap: 1.5,
-              '& .MuiAccordion-root:first-of-type': { borderTopLeftRadius: accordionRadius, borderTopRightRadius: accordionRadius },
-              '& .MuiAccordion-root:last-of-type': { borderBottomLeftRadius: accordionRadius, borderBottomRightRadius: accordionRadius }
+              '& .MuiAccordion-root:first-of-type': {
+                borderTopLeftRadius: accordionRadius,
+                borderTopRightRadius: accordionRadius,
+              },
+              '& .MuiAccordion-root:last-of-type': {
+                borderBottomLeftRadius: accordionRadius,
+                borderBottomRightRadius: accordionRadius,
+              },
             }}
           >
-            {filterFaqList.map((item, index) => (
-              <motion.div
-                key={index}
-                initial={{ y: 20, opacity: 0 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{ duration: 0.2, delay: 0.3 }}
-              >
-                <Accordion
+            {filterFaqList.map((item, index) => {
+              const isExpanded = expanded === `panel${index}`;
+
+              return (
+                <motion.div
                   key={index}
-                  expanded={expanded === `panel${index}`}
-                  onChange={handleChange(`panel${index}`)}
-                  sx={{
-                    borderRadius: cardRadius,
-                    backgroundColor: 'grey.100',
-                    ...(isFocusWithin && { '&:focus-within': generateFocusVisibleStyles(theme.palette.primary.main) })
-                  }}
+                  initial={{ y: 20, opacity: 0 }}
+                  whileInView={{ y: 0, opacity: 1 }}
+                  viewport={{ once: true }}
+                  transition={{ duration: 0.2, delay: 0.3 }}
                 >
-                  <AccordionSummary
-                    expandIcon={<SvgIcon name={expanded === `panel${index}` ? 'tabler-minus' : 'tabler-plus'} {...iconProps} size={20} />}
+                  <Accordion
+                    expanded={isExpanded}
+                    onChange={handleChange(`panel${index}`)}
                     sx={{
-                      p: accordionPX,
-                      '& .MuiAccordionSummary-expandIconWrapper': { color: 'text.primary' },
-                      '& .MuiAccordionSummary-content': { my: 0 },
-                      '&.Mui-focusVisible': { bgcolor: 'transparent' },
-                      '&:hover, &:hover svg': { color: 'primary.dark' }
+                      borderRadius: cardRadius,
+                      backgroundColor: 'grey.100',
+                      backgroundImage: !isExpanded
+                        ? `linear-gradient(to right, ${theme.palette.grey[100]} 30%, rgba(255,255,255,0) 70%), url(${item.image})`
+                        : 'none',
+                      backgroundSize: 'contain',
+                      backgroundPosition: 'calc(100% - 20px) center',
+                      backgroundRepeat: 'no-repeat',
+                      color: 'text.primary',
+                      position: 'relative',
+                      overflow: 'hidden',
+                      height: isExpanded ? 'auto' : '100px',
+                      minHeight: isExpanded ? 'unset' : '100px',
+                      ...(isFocusWithin && {
+                        '&:focus-within': generateFocusVisibleStyles(theme.palette.primary.main),
+                      }),
                     }}
                   >
-                    <Typography variant="h4">{item.question}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails sx={{ px: accordionPX, pt: 0, pb: accordionPX }} key={index}>
-                    <FaqDetails answer={item.answer} />
-                  </AccordionDetails>
-                </Accordion>
-              </motion.div>
-            ))}
+                    <AccordionSummary
+                      expandIcon={
+                        <Stack
+                          sx={{
+                            width: 32,
+                            height: 32,
+                            borderRadius: '50%',
+                            backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                          }}
+                        >
+                          <SvgIcon
+                            name={isExpanded ? 'tabler-minus' : 'tabler-plus'}
+                            color="text.primary"
+                            size={20}
+                          />
+                        </Stack>
+                      }
+                      sx={{
+                        p: accordionPX,
+                        '& .MuiAccordionSummary-expandIconWrapper': { color: 'text.primary' },
+                        '& .MuiAccordionSummary-content': { my: 0 },
+                        '&.Mui-focusVisible': { bgcolor: 'transparent' },
+                        '&:hover, &:hover svg': { color: 'primary.dark' },
+                      }}
+                    >
+                      <Typography variant="h4">{item.question}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails sx={{ px: accordionPX, pt: 0, pb: accordionPX }}>
+                      <FaqDetails answer={item.answer} />
+                    </AccordionDetails>
+                  </Accordion>
+                </motion.div>
+              );
+            })}
           </Stack>
         </Stack>
       </Stack>
@@ -110,9 +154,16 @@ export default function Experiences({ heading, caption, defaultExpanded, faqList
 }
 
 Experiences.propTypes = {
-  heading: PropTypes.any,
-  caption: PropTypes.any,
-  defaultExpanded: PropTypes.any,
-  faqList: PropTypes.any,
-  activeCategory: PropTypes.string
+  heading: PropTypes.string,
+  caption: PropTypes.string,
+  defaultExpanded: PropTypes.string,
+  faqList: PropTypes.arrayOf(
+    PropTypes.shape({
+      question: PropTypes.string.isRequired,
+      answer: PropTypes.string.isRequired,
+      category: PropTypes.string,
+      image: PropTypes.string,
+    })
+  ).isRequired,
+  activeCategory: PropTypes.string,
 };
